aa27d472b7a332ddee27fa3603dd69cbd798f2b3
Shrinav Loka <lokashrinav@gmail.com>
2024-11-02 00:37:24 -0400

    Finished calc mini-project

--- calc.f95
+++ calc.f95
@@ -1,14 +1,26 @@
 program calculator
     implicit none
-    character (len=20) op
-    integer a, b
-    real res
+    character (len=20) :: op
+    integer :: a, b
+    real :: res
     write(*,*) "Insert 'add', 'sub', 'mul', or 'div' follwed by two numbers"
     read(*,*) op, a, b
-    if(op .eqv. 'add') then 
-        res :: a + b
-    elseif(op, .eqv 'sub') then
-        res :: 
-        
+    if(op .eq. 'add') then 
+        res = a + b
+    elseif(op .eq. 'sub') then
+        res = a - b
+    elseif(op .eq. 'mul') then
+        res = a * b
+    elseif(op .eq. 'div') then
+        if(b == 0) then
+            write(*,*) "Can't Input 0 as Denominator"
+            stop
+        else
+            res = a / b
+        end if
+    else
+        write(*,*) "Invalid Operation"
+    end if
+    write(*,*) res
 
 end program calculator
